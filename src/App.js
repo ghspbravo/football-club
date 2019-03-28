403126bf96d2aa54ee09780e0172659c10d62dc0
ghspbravo <gmspbravo@gmail.com>
2019-03-29 02:39:57 +0500

    router configuration in app main

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// components
+import header from './components/header/header';
+
+// pages
+import account from './pages/account/account';
+import inDevelop from './pages/development/inDevelop';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/development" />
+          <Route component={header} />
+        </Switch>
+
+        <Route exact path="/" component={account} />
+        <Route exact path="/development" component={inDevelop} />
+      </Router>
     );
   }
 }
