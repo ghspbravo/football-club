901fc7263d6e7d183e827eca2279761e7e586426
ghspbravo <gmspbravo@gmail.com>
2019-03-29 16:30:42 +0500

    Updates

---
+++ precache-manifest.af025b7c45311785764a2094f813cf07.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "0437afcf3695d6e4c4e5",
+    "url": "/football-club/static/css/main.420fbaf0.chunk.css"
+  },
+  {
+    "revision": "0437afcf3695d6e4c4e5",
+    "url": "/football-club/static/js/main.d7400237.chunk.js"
+  },
+  {
+    "revision": "b486ee6cff92e8de1d74",
+    "url": "/football-club/static/js/runtime~main.cdd81b9b.js"
+  },
+  {
+    "revision": "0a4e1f9dabb5e4d54227",
+    "url": "/football-club/static/js/2.32ec1387.chunk.js"
+  },
+  {
+    "revision": "6c4b54832c1260aba40b3a2b5cd39bd7",
+    "url": "/football-club/static/media/avatar.6c4b5483.jpg"
+  },
+  {
+    "revision": "ca0a8cf8bf7a6e6a8265c960ddb5743b",
+    "url": "/football-club/static/media/avatar.ca0a8cf8.png"
+  },
+  {
+    "revision": "87a8392b30b2ce9fc3ad996bc03799de",
+    "url": "/football-club/static/media/logo.87a8392b.png"
+  },
+  {
+    "revision": "f7548f0f2936703de9253061df8a3db9",
+    "url": "/football-club/static/media/feed6.f7548f0f.jpg"
+  },
+  {
+    "revision": "2abe0f05d19a4233790e7fc812724beb",
+    "url": "/football-club/static/media/feed7.2abe0f05.jpg"
+  },
+  {
+    "revision": "832a4d3ff41654d5d835bad73c2784d2",
+    "url": "/football-club/static/media/bg.832a4d3f.jpg"
+  },
+  {
+    "revision": "34061bee84b16372b2a4d0ff33d7a131",
+    "url": "/football-club/static/media/project-card6.34061bee.jpg"
+  },
+  {
+    "revision": "e6e29360f7a314fcbae265724320a472",
+    "url": "/football-club/static/media/browse1.e6e29360.jpg"
+  },
+  {
+    "revision": "9fc4b0f46c9c39d6d52782ec20f6cea8",
+    "url": "/football-club/static/media/app-icon5.9fc4b0f4.png"
+  },
+  {
+    "revision": "c3b8afdda496dce8d69e3ab85e0b372a",
+    "url": "/football-club/index.html"
+  }
+];
